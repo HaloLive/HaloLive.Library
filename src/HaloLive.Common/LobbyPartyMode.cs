e83e2bc1f708db73a139b4e7cfb6530bbab68aa3
HelloKitty <andrew.blakely@ymail.com>
2017-06-17 16:21:55 -0500

    Added LobbyPartyMode

---
+++ src/HaloLive.Common/LobbyPartyMode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaloLive.Common
+{
+	/// <summary>
+	/// Enumeration of valid party modes for an Xboxlive party.
+	/// </summary>
+	public enum LobbyPartyMode : int
+	{
+		/// <summary>
+		/// Indicates that the lobby is open to all.
+		/// </summary>
+		OpenParty = 0,
+
+		/// <summary>
+		/// Indicates that this is a friends only lobby.
+		/// </summary>
+		FriendsOnly = 1,
+
+		/// <summary>
+		/// Indicates that the lobby can only be joined by invite.
+		/// </summary>
+		InviteOnly = 2,
+
+		//TODO: What is this? Local?
+		Online = 3,
+
+		//TODO: What is this? Local?
+		Offline = 4
+	}
+}
