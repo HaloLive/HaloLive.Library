46951f1b14e9f945ccdfcc7d49b9c525c5dc6c57
HelloKitty <andrew.blakely@ymail.com>
2017-06-17 16:21:47 -0500

    Added LobbyType

---
+++ src/HaloLive.Common/LobbyType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaloLive.Common
+{
+	/// <summary>
+	/// Enumeration of the lobby types in Halo.
+	/// </summary>
+	public enum LobbyType : int
+	{
+		/// <summary>
+		/// The campaign lobby.
+		/// </summary>
+		Campaign = 0,
+
+		/// <summary>
+		/// The Xboxlive matchmaking lobby.
+		/// </summary>
+		Matchmaking = 1,
+
+		//TODO: What is this really? Maybe custom games?
+		/// <summary>
+		/// Systemlink lobby? Not sure.
+		/// </summary>
+		Multiplayer = 2,
+
+		/// <summary>
+		/// The Forge lobby.
+		/// </summary>
+		Forge = 3,
+
+		/// <summary>
+		/// The replay theater lobby.
+		/// </summary>
+		Theater = 4
+	}
+}
