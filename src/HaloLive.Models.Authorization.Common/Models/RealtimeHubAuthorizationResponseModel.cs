f4c8c18295d195ac792844a1d7d8d163012729c5
HelloKitty <andrew.blakely@ymail.com>
2017-06-20 16:28:49 -0500

    Fixed Authorization project location

---
+++ src/HaloLive.Models.Authorization.Common/Models/RealtimeHubAuthorizationResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HaloLive.Models.Authorization
+{
+	/// <summary>
+	/// Model/response sent back as a response to the <see cref="RealtimeHubAuthorizationRequestModel"/>.
+	/// </summary>
+	[JsonObject]
+	public sealed class RealtimeHubAuthorizationResponseModel
+	{
+		
+	}
+}
