9f457bae98781fbefddcc51d6a6567c3379ba475
HelloKitty <andrew.blakely@ymail.com>
2017-06-20 10:38:02 -0500

    Reoganize auth models into models proj

---
+++ src/HaloLive.Metadata.Common/RequiresAuthorizationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaloLive.Metadata.Common
+{
+	/// <summary>
+	/// Meta-data marker to indicate that a model requires authorization.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public sealed class RequiresAuthorizationAttribute : Attribute
+	{
+		public RequiresAuthorizationAttribute()
+		{
+			
+		}
+	}
+}
--- tests/HaloLive.Models.Tests/Properties/AssemblyInfo.cs
+++ tests/HaloLive.Models.Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HaloLive.Network.Common.Tests")]
+[assembly: AssemblyTitle("HaloLive.Models.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("HaloLive.Network.Common.Tests")]
+[assembly: AssemblyProduct("HaloLive.Models.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("f7814015-1635-4638-8f7b-655d5678bfe8")]
+[assembly: Guid("6c3814d6-628e-4fb7-b295-a039482dec1d")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
