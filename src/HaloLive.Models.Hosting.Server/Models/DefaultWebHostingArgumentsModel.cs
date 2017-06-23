e177266b5a23b8ef37014e444b992d9740cb26a7
HelloKitty <andrew.blakely@ymail.com>
2017-06-23 14:49:35 -0500

    Added DefaultWebHostingArgumentsModel

---
+++ src/HaloLive.Models.Hosting.Server/Models/DefaultWebHostingArgumentsModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace HaloLive.Models.Hosting
+{
+	/// <summary>
+	/// Default command line hosting options for HaloLive web services.
+	/// </summary>
+	public sealed class DefaultWebHostingArgumentsModel
+	{
+		/// <summary>
+		/// Command line option --url={url}.
+		/// Specifies the URL {url} to host the web service on.
+		/// </summary>
+		[Option('u', "url")]
+		public string Url { get; private set; } //For CommandLineParser 2.1.1-beta we must have private setters
+
+		/// <summary>
+		/// Indicates if a custom URL is defined.
+		/// </summary>
+		public bool isCustomUrlDefined => !String.IsNullOrEmpty(Url);
+
+		/// <summary>
+		/// Command line options --usehttps={CertName}.
+		/// Indicates if HTTPS should be enabled and what cert to use.
+		/// </summary>
+		[Option('h', "usehttps")]
+		public string HttpsCertificateName { get; private set; } //For CommandLineParser 2.1.1-beta we must have private setters
+
+		/// <summary>
+		/// Indicates if https has been enabled with a custom cert defined.
+		/// </summary>
+		public bool isHttpsEnabled => !String.IsNullOrEmpty(HttpsCertificateName);
+
+		/// <summary>
+		/// Command line options --usejwt={CertName}.
+		/// Indicates if a JWT will ever be used or parsed and should be enabled.
+		/// </summary>
+		[Option('j', "usejwt")]
+		public string JwtCertificateName { get; private set; } //For CommandLineParser 2.1.1-beta we must have private setters
+
+		/// <summary>
+		/// Indicates if JWT handling has been enabled with a custom cert defined.
+		/// </summary>
+		public bool isJwtEnabled => !String.IsNullOrEmpty(JwtCertificateName);
+	}
+}
---
+++ tests/HaloLive.Models.Tests/UnitTests/DefaultWebHostingArgumentsModelTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using HaloLive.Models.Hosting;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace HaloLive.Models.Tests.UnitTests
+{
+	[TestFixture]
+	public static class DefaultWebHostingArgumentsModelTests
+	{
+		[Test]
+		public static void Test_Can_Parse_From_Empty_Args()
+		{
+			//arrange
+			string[] args = new string[0];
+
+			//act
+			Parser.Default.ParseArguments<DefaultWebHostingArgumentsModel>(args);
+			//.WithParsed(model => Assert.Fail("There are no parsed options. It should never reach here."))
+			//.WithNotParsed(errors => Assert.Fail("There should be no incorrectly parsed options. It should never reach here."));
+		}
+
+		[Test]
+		[TestCase()]
+		[TestCase("", "hi", "test", "   ")]
+		public static void Test_Can_Parse_From_Empty_Args_Without_Producing_Invalid_State(params string[] args)
+		{
+			//act
+			Parser.Default.ParseArguments<DefaultWebHostingArgumentsModel>(args)
+				.WithParsed(model => Assert.False(model.isCustomUrlDefined || model.isHttpsEnabled || model.isJwtEnabled))
+				.WithNotParsed(Assert.IsEmpty);
+		}
+
+		[Test]
+		[TestCase("--usehttps=TestCert.pfx")]
+		public static void Test_Can_Parse_Https_Option_from_Args(params string[] args)
+		{
+			//assert
+			Parser.Default.ParseArguments<DefaultWebHostingArgumentsModel>(args)
+				.WithParsed(model =>
+				{
+					Assert.False(model.isCustomUrlDefined || model.isJwtEnabled || !model.isHttpsEnabled);
+					Assert.AreEqual("TestCert.pfx", model.HttpsCertificateName);
+				})
+				.WithNotParsed(errors => Assert.IsEmpty(errors, $"Errors {errors.Aggregate("", (s, error) => $"{s}\n{error}")}"));
+		}
+
+		[Test]
+		[TestCase(@"--url=http://127.0.0.1:80")]
+		public static void Test_Can_Parse_Url_Option_from_Args(params string[] args)
+		{
+			//assert
+			Parser.Default.ParseArguments<DefaultWebHostingArgumentsModel>(args)
+				.WithParsed(model =>
+				{
+					Assert.False(model.isHttpsEnabled || model.isJwtEnabled || !model.isCustomUrlDefined);
+					Assert.AreEqual(@"http://127.0.0.1:80", model.Url);
+				})
+				.WithNotParsed(errors => Assert.IsEmpty(errors, $"Errors {errors.Aggregate("", (s, error) => $"{s}\n{error}")}"));
+		}
+
+		[Test]
+		[TestCase("--usejwt=TestCert.pfx")]
+		public static void Test_Can_Parse_Jwt_Option_from_Args(params string[] args)
+		{
+			//assert
+			Parser.Default.ParseArguments<DefaultWebHostingArgumentsModel>(args)
+				.WithParsed(model =>
+				{
+					Assert.False(model.isCustomUrlDefined || model.isHttpsEnabled || !model.isJwtEnabled);
+					Assert.AreEqual("TestCert.pfx", model.JwtCertificateName);
+				})
+				.WithNotParsed(errors => Assert.IsEmpty(errors, $"Errors {errors.Aggregate("", (s, error) => $"{s}\n{error}")}"));
+		}
+
+		[Test]
+		[TestCase("--usejwt=TestCert.pfx", @"--url=http://127.0.0.1:80", "--usehttps=TestCert.pfx")]
+		public static void Test_Can_Parse_All_Option_from_Args(params string[] args)
+		{
+			//assert
+			Parser.Default.ParseArguments<DefaultWebHostingArgumentsModel>(args)
+				.WithParsed(model =>
+				{
+					Assert.True(model.isCustomUrlDefined && model.isHttpsEnabled && model.isJwtEnabled);
+					Assert.AreEqual("TestCert.pfx", model.JwtCertificateName);
+					Assert.AreEqual(@"http://127.0.0.1:80", model.Url);
+					Assert.AreEqual("TestCert.pfx", model.HttpsCertificateName);
+				})
+				.WithNotParsed(errors => Assert.IsEmpty(errors, $"Errors {errors.Aggregate("", (s, error) => $"{s}\n{error}")}"));
+		}
+	}
+}
