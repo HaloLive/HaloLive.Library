e54ce0fc201a2d4079ab3cacc734747138d9219b
HelloKitty <andrew.blakely@ymail.com>
2017-06-23 13:01:24 -0500

    Added the tests missing form last commit

---
+++ tests/HaloLive.Models.Tests/UnitTests/NameEndpointResolutionStorageModelTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HaloLive.Models.NameResolution;
+using HaloLive.Network.Common;
+using Newtonsoft.Json;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace HaloLive.Models.Tests.UnitTests
+{
+	[TestFixture]
+	public static class NameEndpointResolutionStorageModelTests
+	{
+		[Test]
+		[TestCase((ClientRegionLocale) int.MaxValue)]
+		[TestCase((ClientRegionLocale) 55555)]
+		[TestCase((ClientRegionLocale) (-5))]
+		public static void Test_Throws_On_Construction_With_Invalid_Argument_ServiceType(ClientRegionLocale value)
+		{
+			//assert
+			Assert.Throws<ArgumentOutOfRangeException>(() => new NameEndpointResolutionStorageModel(value, new Dictionary<NetworkServiceType, ResolvedEndpoint>()));
+		}
+
+		[Test]
+		[TestCase(ClientRegionLocale.CN)]
+		[TestCase(ClientRegionLocale.EU)]
+		[TestCase(ClientRegionLocale.KR)]
+		[TestCase(ClientRegionLocale.RU)]
+		public static void Test_Throws_On_Construction_With_Invalid_Argument_Dictionary(ClientRegionLocale value)
+		{
+			//assert
+			Assert.Throws<ArgumentNullException>(() => new NameEndpointResolutionStorageModel(value, null));
+		}
+
+		[Test]
+		[TestCase(ClientRegionLocale.CN)]
+		[TestCase(ClientRegionLocale.EU)]
+		[TestCase(ClientRegionLocale.KR)]
+		[TestCase(ClientRegionLocale.RU)]
+		public static void Test_Doesnt_Throw_On_Valid_Arguments(ClientRegionLocale value)
+		{
+			//assert
+			Assert.DoesNotThrow(() => new NameEndpointResolutionStorageModel(value, new Dictionary<NetworkServiceType, ResolvedEndpoint>()));
+		}
+
+		[Test]
+		[TestCase(ClientRegionLocale.CN)]
+		[TestCase(ClientRegionLocale.EU)]
+		[TestCase(ClientRegionLocale.KR)]
+		[TestCase(ClientRegionLocale.RU)]
+		public static void Test_Can_JSON_Serialize_To_NonNull_Non_Whitespace(ClientRegionLocale value)
+		{
+			//arrange
+			Dictionary<NetworkServiceType, ResolvedEndpoint> endpoints = new Dictionary<NetworkServiceType, ResolvedEndpoint>()
+			{
+				{NetworkServiceType.AuthenticationService, new ResolvedEndpoint("127.0.0.1", 5555)}
+			};
+			NameEndpointResolutionStorageModel model = new NameEndpointResolutionStorageModel(value, endpoints);
+
+			//act
+			string serializedModel = JsonConvert.SerializeObject(model);
+
+			//assert
+			Assert.NotNull(serializedModel);
+			Assert.IsNotEmpty(serializedModel);
+			Assert.True(serializedModel.Contains(endpoints.Values.First().EndpointAddress));
+			Assert.True(serializedModel.Contains(nameof(model.ServiceEndpoints)));
+			Assert.True(serializedModel.Contains(endpoints.Values.First().EndpointPort.ToString()));
+			Assert.True(serializedModel.Contains(((int) value).ToString()));
+		}
+
+		[Test]
+		[TestCase(ClientRegionLocale.CN)]
+		[TestCase(ClientRegionLocale.EU)]
+		[TestCase(ClientRegionLocale.KR)]
+		[TestCase(ClientRegionLocale.RU)]
+		public static void Test_Can_JSON_Serialize_Then_Deserialize_With_Preserved_Values(ClientRegionLocale value)
+		{
+			//arrange
+			Dictionary<NetworkServiceType, ResolvedEndpoint> endpoints = new Dictionary<NetworkServiceType, ResolvedEndpoint>()
+			{
+				{NetworkServiceType.AuthenticationService, new ResolvedEndpoint("127.0.0.1", 5555)}
+			};
+			NameEndpointResolutionStorageModel model = new NameEndpointResolutionStorageModel(value, endpoints);
+
+			//act
+			NameEndpointResolutionStorageModel deserializedModel =
+				JsonConvert.DeserializeObject<NameEndpointResolutionStorageModel>(JsonConvert.SerializeObject(model));
+
+			//assert
+			Assert.NotNull(deserializedModel);
+			Assert.NotNull(deserializedModel.ServiceEndpoints);
+			Assert.IsNotEmpty(deserializedModel.ServiceEndpoints);
+			Assert.True(Enum.IsDefined(typeof(ClientRegionLocale), deserializedModel.Region));
+			Assert.True(Enum.IsDefined(typeof(NetworkServiceType), deserializedModel.ServiceEndpoints.Keys.First()));
+			Assert.AreEqual(endpoints.First().Key, deserializedModel.ServiceEndpoints.First().Key);
+			Assert.AreEqual(endpoints.First().Value.EndpointAddress, deserializedModel.ServiceEndpoints.First().Value.EndpointAddress);
+		}
+	}
+}
