4c16005124495f4374906fcda59f59720c48ba2f
HelloKitty <andrew.blakely@ymail.com>
2017-06-24 13:46:55 -0500

    Added Identity models for Authentication

---
+++ src/HaloLive.Models.Authentication.Server/Models/HaloLiveApplicationRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace HaloLive.Models.Authentication
+{
+	/// <summary>
+	/// HaloLive OpenIddict app role.
+	/// See Documentation for details: https://github.com/openiddict/openiddict-core
+	/// </summary>
+	public class HaloLiveApplicationRole : IdentityRole<int> { } //we don't need any additional data; we rely directly on identity
+}
---
+++ src/HaloLive.Models.Authentication.Server/Models/HaloLiveApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace HaloLive.Models.Authentication
+{
+	/// <summary>
+	/// HaloLive OpenIddict app user.
+	/// See Documentation for details: https://github.com/openiddict/openiddict-core
+	/// </summary>
+	public class HaloLiveApplicationUser : IdentityUser<int> { } //we don't need any additional data; we rely directly on identity
+}
