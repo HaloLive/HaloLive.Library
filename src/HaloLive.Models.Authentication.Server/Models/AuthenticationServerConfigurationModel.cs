d901fd015c2bd9396a31b855921694641b637369
HelloKitty <andrew.blakely@ymail.com>
2017-06-24 14:50:10 -0500

    Added missed dbstring to auth config

--- src/HaloLive.Models.Authentication.Server/Models/AuthenticationServerConfigurationModel.cs
+++ src/HaloLive.Models.Authentication.Server/Models/AuthenticationServerConfigurationModel.cs
@@ -27,17 +27,26 @@ namespace HaloLive.Models.Authentication
 		public string JwtSigningX509Certificate2Path { get; }
 
 		/// <summary>
+		/// Should be the database string that will allow the authentication service to connect to the table/database
+		/// containing the auth tables.
+		/// </summary>
+		[JsonProperty(Required = Required.Always)]
+		public string AuthenticationDatabaseString { get; }
+
+		/// <summary>
 		/// Creates a new configuration for the authentication server.
 		/// </summary>
 		/// <param name="endpoint">The endpoint/controller/action used for authentication.</param>
 		/// <param name="certPath">The path to the JWT signing certificate.</param>
-		public AuthenticationServerConfigurationModel(string endpoint, string certPath)
+		public AuthenticationServerConfigurationModel(string endpoint, string certPath, string authDbString)
 		{
 			if (string.IsNullOrWhiteSpace(endpoint)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(endpoint));
 			if (string.IsNullOrWhiteSpace(certPath)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(certPath));
+			if (string.IsNullOrWhiteSpace(authDbString)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(authDbString));
 
 			AuthenticationControllerEndpoint = endpoint;
 			JwtSigningX509Certificate2Path = certPath;
+			AuthenticationDatabaseString = authDbString;
 		}
 
 		/// <summary>
