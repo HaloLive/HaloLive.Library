5b110d721eb17591ce92da68dab306fee60c764d
HelloKitty <andrew.blakely@ymail.com>
2017-06-25 12:30:44 -0500

    Added Authorization base controller

---
+++ src/HaloLive.Hosting.Authorization.Server/Controllers/AuthorizationReadyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HaloLive.Models.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaloLive.Hosting
+{
+	/// <summary>
+	/// Base Halo Live MVC controller that is ready to handle authorization related functions.
+	/// </summary>
+	public abstract class AuthorizationReadyController : Controller
+	{
+		//Use GetUserId to get the HaloLive account ID.
+		//USe GetUserName to get the HaloLive account name.
+		protected UserManager<HaloLiveApplicationUser> HaloLiveUserManager { get; }
+
+		/// <summary>
+		/// Creates a new authorization ready controller.
+		/// </summary>
+		/// <param name="haloLiveUserManager">The user manager to be used for identity/user authorization functions.</param>
+		protected AuthorizationReadyController([FromServices] UserManager<HaloLiveApplicationUser> haloLiveUserManager)
+		{
+			if (haloLiveUserManager == null) throw new ArgumentNullException(nameof(haloLiveUserManager));
+
+			//We NEED the usermanager because the easiest and cleanest way to access the identity claims such as ID is through
+			//the user manager
+			HaloLiveUserManager = haloLiveUserManager;
+		}
+	}
+}
