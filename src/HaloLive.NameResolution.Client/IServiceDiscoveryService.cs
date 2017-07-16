7cbd51e5df41383e599f1cb94a1e9a4772478593
HelloKitty <andrew.blakely@ymail.com>
2017-07-16 05:23:19 -0500

    Added Service discovery client

---
+++ src/HaloLive.NameResolution.Client/IServiceDiscoveryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HaloLive.Models.NameResolution;
+using TypeSafe.Http.Net;
+
+namespace HaloLive.Network
+{
+	public interface IServiceDiscoveryService
+	{
+		/// <summary>
+		/// Attempts to discover the service specified in the <see cref="ResolveServiceEndpointRequestModel"/>.
+		/// </summary>
+		/// <param name="request">The request.</param>
+		/// <returns>The result of the resolution request.</returns>
+		[Post("/api/ServiceDiscovery/Discover")]
+		Task<ResolveServiceEndpointResponseModel> Discover([JsonBody] ResolveServiceEndpointRequestModel request);
+	}
+}
