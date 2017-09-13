5cc8ca3ff1da83d3156049a0ec8e728da42c8c6e
HelloKitty <andrew.blakely@ymail.com>
2017-09-13 18:56:08 -0500

    Added claims principal extensions

---
+++ src/HaloLive.Hosting.Authorization.Server/Extensions/ClaimsPrincipalReaderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace HaloLive.Hosting.Authorization
+{
+	public static class ClaimsPrincipalReaderExtensions
+	{
+		/// <summary>
+		/// Returns the User ID claim value if present otherwise will throw.
+		/// </summary>
+		/// <param name="principal">The <see cref="ClaimsPrincipal"/> instance.</param>
+		/// <returns>The User ID claim value. Will throw if the principal doesn't contain the id.</returns>
+		/// <exception cref="ArgumentException">Throws if the provided principal doesn't contain an id.</exception>
+		/// <remarks>The User ID claim is identified by <see cref="ClaimTypes.NameIdentifier"/>.</remarks>
+		public static int GetUserIdInt(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
+		{
+			int accountId;
+			if(!int.TryParse(reader.GetUserId(principal), out accountId))
+			{
+				throw new ArgumentException($"Provided {nameof(ClaimsPrincipal)} does not contain a user id.", nameof(principal));
+			}
+
+			return accountId;
+		}
+	}
+}
