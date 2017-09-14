dcf7397e374f250c26b6e0303ddde8cb8eab2a6a
HelloKitty <andrew.blakely@ymail.com>
2017-09-13 19:16:51 -0500

    Removed explict nuget file in myget build

--- MyGet.bat
+++ MyGet.bat
@@ -1,2 +1,2 @@
-%NUGET% restore HaloLive.Library.sln -NoCache -NonInteractive -ConfigFile Nuget.config
+%NUGET% restore HaloLive.Library.sln -NoCache -NonInteractive
 msbuild HaloLive.Library.sln /p:Configuration=Release
